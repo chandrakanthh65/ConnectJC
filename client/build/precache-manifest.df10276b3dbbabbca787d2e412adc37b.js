d0b545514d7eb4ed18eb648f93a6af52cfe2f2be
Ranjan14 <63440245+Ranjan14@users.noreply.github.com>
2021-07-22 10:55:03 +0530

    Add files via upload

---
+++ client/build/precache-manifest.df10276b3dbbabbca787d2e412adc37b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f3664765fd4b8b61408ba0bf4d657dc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "49488d3f8f42eac951c6",
+    "url": "/static/css/main.7ed81f64.chunk.css"
+  },
+  {
+    "revision": "a9fe1f02dac5f5bcc28f",
+    "url": "/static/js/2.e14969b4.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.e14969b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49488d3f8f42eac951c6",
+    "url": "/static/js/main.9eaf7dc7.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
