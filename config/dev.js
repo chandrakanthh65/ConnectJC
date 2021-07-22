46608d69276dff28686d042880bc37d923f6a74d
Ranjan14 <63440245+Ranjan14@users.noreply.github.com>
2021-07-22 10:33:18 +0530

    deletng the previous

--- config/dev.js
+++
@@ -1,5 +0,0 @@
-module.exports={
-    MONGOURI:"mongodb+srv://new-user-chandrakanth:ERul0Vxz61JICWGE@cluster0.c21zu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    JWT_SECRET:"ffdyuuytuyvngrujguywgj",
-    SENDGRID_API:"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"
-}
